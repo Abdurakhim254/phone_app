13e93e6685b97c2fa4c947cf2141251ce2abf018
Abdurakxim254 <toxirmalk@gmail.com>
2025-01-15 14:24:23 +0500

    Just Test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { loginAuthdto } from './dto/login-dto';
 
 @Injectable()
-export class AuthService {
+export class AuthService {                             
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
@@ -11,6 +11,4 @@ export class AuthService {
   loginservice(loginauthdto:loginAuthdto){
     return loginauthdto
   }
-
-  u
 }
